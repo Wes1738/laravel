53511f5a586cfeeffe87d7a833fb504bb3840905
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-08-25 14:05:24 -0300

    Rotas com parâmetros no Laravel

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,15 @@ Route::get('/any', function () {
 Route::match(['get', 'post'], '/match', function () {
     return 'match';
 });
+// Rotas com parâmetros no Laravel
+Route::get('/categorias/{slug}', function ($slug) {
+    return "Produtos da Categoria: {$slug}";
+});
+//Observe = Nome da Rota/Valor dinâmico/Prefixo
+Route::get('/categoria/{slug}/posts', function ($slug) {
+    return "Posts da Categoria: {$slug}";
+});
+// Parâmetros Opcionais (pode ter prefixo também se quiser => '/produtos/{idProduct?}/algumacoisa')
+Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
+    return "Produto(s) {$idProduct}";
+});
