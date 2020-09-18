5df492a2ec2a3a2e812d002a5acbc276ce7ba668
Wesley dos Santos <wesleyrodriguessantos99@gmail.com>
2020-09-17 21:59:42 -0300

    Assets no Laravel

---
+++ laraveltips/ep10/resources/views/admin/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Painel de Administração</title>
+    <link rel="stylesheet" href="{{url(mix('admin/css/stylesADM.css'))}}">
+</head>
+<body>
+    
+    <h1>Painel de Administração - UpInside</h1>
+
+<script src="{{ url(mix('admin/js/appADM.js'))}}"></script>
+</body>
+</html>
---
+++ laraveltips/ep10/resources/views/site/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UpInside Treinamentos</title>
+
+<link rel="stylesheet" href="{{ url(mix('site/css/styles.css'))}}">
+</head>
+<body>
+    
+    <h1>UpInside Treinamentos - Laravel Tips</h1>
+
+<script src="{{ url(mix('site/js/app.js'))}}"></script>
+</body>
+</html>
